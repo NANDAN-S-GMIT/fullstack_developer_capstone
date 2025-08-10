6b6f713d5ace93cb1ae1858ef6e86bf2e5f0043a
NANDAN-S-GMIT <benz.nandan.s@gmail.com>
2025-08-10 21:01:58 +0530

    Updated full project

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,13 +1,26 @@
-# from django.contrib import admin
-# from .models import related models
-
-
-# Register your models here.
+from django.contrib import admin
+from .models import CarMake, CarModel
 
 # CarModelInline class
 
+
+class CarModelInline(admin.TabularInline):
+    model = CarModel
+    extra = 1
+
 # CarModelAdmin class
+
+
+class CarModelAdmin(admin.ModelAdmin):
+    pass  # Add any customizations if needed
 
 # CarMakeAdmin class with CarModelInline
 
+
+class CarMakeAdmin(admin.ModelAdmin):
+    inlines = [CarModelInline]
+
+
 # Register models here
+admin.site.register(CarMake, CarMakeAdmin)
+admin.site.register(CarModel, CarModelAdmin)
